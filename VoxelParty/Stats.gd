616bd4ffc1666bc260f52a42181de090e88b9f44
lavaman11 <lavaman11@users.noreply.github.com>
2020-05-26 21:19:37 +0200

    update

--- VoxelParty/Stats.gd
+++ VoxelParty/Stats.gd
@@ -3,6 +3,9 @@ extends Node
 export(int) var max_health = 1 setget set_max_health
 var health = max_health setget set_health
 
+const Restart = preload("res://Player/Restart.tscn")
+const Player = preload("res://Player/Player.tscn")
+
 signal no_health
 signal health_changed(value)
 signal max_health_changed(value)
@@ -17,6 +20,15 @@ func set_health(value):
 	emit_signal("health_changed", health)
 	if health <= 0:
 		emit_signal("no_health")
-
+		var restart = Restart.instance()
+		get_parent().add_child(restart)
 func _ready():
 	self.health = max_health
+
+
+func _on_TextureButton_pressed():
+	if Input.action_press("restart"):
+		var player = Player.instance()
+		get_parent().add_child(player)
+		
+	
